756f2adb686f96e3dcc4f3f829457ab07da1277d
Katrina R <katrinarosales@live.com>
2017-01-28 23:28:59 +0100

    Forgot to add AdressViewModel in our new VIewModels2

---
+++ Gruppuppgift-Webprojekt-master/Webb4 DAL/ViewModels2/VyModels/AdressViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webb4_DAL.ViewModels2
+{
+    public class AdressViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public int PostalNr { get; set; }
+        public string City { get; set; }
+    }
+}
