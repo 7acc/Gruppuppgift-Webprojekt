040ecbca02dd3c6b73f975eea0e8bb96b3c5596c
astromberg1 <astromberg1@hotmail.com>
2017-02-01 19:11:09 +0100

    denna funkar bra för mig

--- Gruppuppgift-Webprojekt-master/Webb4 DAL/Webb4-businesslayer/HelpMapper/AppartmentMapping.cs
+++ Gruppuppgift-Webprojekt-master/Webb4 DAL/Webb4-businesslayer/HelpMapper/AppartmentMapping.cs
@@ -10,48 +10,52 @@ using Webb4_DAL.ModelsV2;
 using Webb4_DAL;
 
 namespace Webb4_businesslayer.HelpMapper
-{
-    public class AppartmentMapping
     {
+    public class AppartmentMapping
+        {
         // AppartmentDataModelRepository<ApartmentData> _genericRepository = new AppartmentDataModelRepository<ApartmentData>(new Webb4Context());
         GenericRepository<Appartment> _genericRepository = new GenericRepository<Appartment>(new Webb4Context());
 
         public List<AppartmentViewModel> FromBltoUiGetAll()
-        {
+            {
             var getData = _genericRepository.GetAll()
                 .Include("Adress").Include("Housing").Include("District")
-                .Select(p => new AppartmentViewModel()
-                {
-                    Id = p.Id,
-                    Area = p.Area,
-                    Rent = p.Rent,
-                    NrOfRooms = p.NrOfRooms,
-                    BuildinFloors = p.BuildinFloors,
-                    Floor = p.Floor,
-                    BuildYear = p.BuildYear,
-                    MoveInDate = p.MoveInDate,
-                    LastAdmissionDate = p.LastAdmissionDate,
-                    PublicationDate = p.PublicationDate,
-                    Avalible = p.Avalible,
-                    //Adress = new AdressViewModel() { Id = p.Adress.Id, Street = p.Adress.Street, City = p.Adress.City, PostalNr = p.Adress.PostalNr },
-                    //Housing = new HousingTypeViewModel() { Id = p.Housing.Id, Type = p.Housing.Type },
-                    //District = new DistrictViewModel() { Id = p.District.Id, Name = p.District.Name, Description = p.District.Description }
+                //.Select(p => new AppartmentViewModel()
+                //{
+                //    Id = p.Id,
+                //    Area = p.Area,
+                //    Rent = p.Rent,
+                //    NrOfRooms = p.NrOfRooms,
+                //    BuildinFloors = p.BuildinFloors,
+                //    Floor = p.Floor,
+                //    BuildYear = p.BuildYear,
+                //    MoveInDate = p.MoveInDate,
+                //    LastAdmissionDate = p.LastAdmissionDate,
+                //    PublicationDate = p.PublicationDate,
+                //    Avalible = p.Avalible,
+                //    //Adress = new AdressViewModel() { Id = p.Adress.Id, Street = p.Adress.Street, City = p.Adress.City, PostalNr = p.Adress.PostalNr },
+                //    //Housing = new HousingTypeViewModel() { Id = p.Housing.Id, Type = p.Housing.Type },
+                //    //District = new DistrictViewModel() { Id = p.District.Id, Name = p.District.Name, Description = p.District.Description }
 
-                })
+                //})
                 //.ProjectTo<AppartmentViewModel>()
+
                 .ToList();
 
-            //var randomItem = Mapper.Map<List<Appartment>, List<AppartmentViewModel>>(getData);
-            return getData;
-        }
+            var randomItem = Mapper.Map<List<Appartment>, List<AppartmentViewModel>>(getData);
+            return randomItem;
+            }
 
         public List<AppartmentViewModel> FromBlToUiGetApartList()
-        {
-           
-            var getData = _genericRepository.GetAll().Include("Adress")
-                .Include("Housing").Include("District")
+            {
+
+            var getData = _genericRepository.GetAll()
+               //.Include(a => new Adress() {Id = a.Adress.Id, Street = a.Adress.Street})
+               //.Include(a => new HousingType() {Id = a.Housing.Id, Type = a.Housing.Type})
+               //.Include(a => new District() {Id = a.District.Id, Name = a.District.Name})
+               //.Include(x => x.Features.Select(s => new Features() { Id = s.Id, name = s.name }))
                .Select(p => new AppartmentViewModel()
-               {
+                   {
                    Id = p.Id,
                    Area = p.Area,
                    Rent = p.Rent,
@@ -63,37 +67,40 @@ namespace Webb4_businesslayer.HelpMapper
                    LastAdmissionDate = p.LastAdmissionDate,
                    PublicationDate = p.PublicationDate,
                    Avalible = p.Avalible,
-                   
-               })
+                   Adress = new AdressViewModel() { Id = p.Adress.Id, Street = p.Adress.Street },
+                   Housing = new HousingTypeViewModel() { Id = p.Housing.Id, Type = p.Housing.Type },
+                   District = new DistrictViewModel() { Id = p.District.Id, Name = p.District.Name, Description = p.District.Description }
+
+                   })
                 .ToList();
             return getData;
-        } 
-        public  async Task<AppartmentViewModel> FromBltoUiGetById(int id)
-        {
+            }
+        public async Task<AppartmentViewModel> FromBltoUiGetById(int id)
+            {
             var getRepo = await _genericRepository.GetByIdAsync(id);
             var detailsId = Mapper.Map<Appartment, AppartmentViewModel>(getRepo);
             return detailsId;
-        }
+            }
 
-        public  async Task FromBltoUiInser(AppartmentViewModel Appart)
-        {
+        public async Task FromBltoUiInser(AppartmentViewModel Appart)
+            {
             var addMap = Mapper.Map<AppartmentViewModel, Appartment>(Appart);
             await _genericRepository.InsertAsync(addMap);
 
-        }
+            }
 
-        public  async Task FromBltoUiEditAsync(AppartmentViewModel Appart)
-        {
+        public async Task FromBltoUiEditAsync(AppartmentViewModel Appart)
+            {
             var editMap = Mapper.Map<AppartmentViewModel, Appartment>(Appart);
             await _genericRepository.EditAsync(editMap);
 
-        }
+            }
 
-        public  async Task FromBltoUiDeleteAsync(int id)
-        {
+        public async Task FromBltoUiDeleteAsync(int id)
+            {
             var getFromR = await _genericRepository.GetByIdAsync(id);
             await _genericRepository.DeleteAsync(getFromR);
 
+            }
         }
     }
-}
