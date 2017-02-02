8cbb4228ac87b21718da1f41c4a3ea8af7ea836a
7acc <jacc321@gmail.com>
2017-02-02 17:12:24 +0100

    custom errors

--- Gruppuppgift-Webprojekt-master/Webb4 DAL/Webb4 - MVC/Global.asax.cs
+++ Gruppuppgift-Webprojekt-master/Webb4 DAL/Webb4 - MVC/Global.asax.cs
@@ -1,8 +1,11 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Webb4_businesslayer;
 using Webb4_businesslayer.AutoMapper;
+using Webb4___MVC.Controllers;
 
 namespace Webb4___MVC
 {
@@ -15,12 +18,46 @@ namespace Webb4___MVC
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            
+
             AutoMapperConfig.RegisterMappings();
 
 
             System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier =
-          System.Security.Claims.ClaimTypes.NameIdentifier;
+                System.Security.Claims.ClaimTypes.NameIdentifier;
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Response.TrySkipIisCustomErrors = true;
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+
+            if (httpException != null)
+            {
+                string action;
+
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        // page not found
+                        action = "NotFound";
+                        break;
+                    case 500:
+                        // server error
+                        action = "ServerError";
+                        break;
+                    default:
+                        action = "Error";
+                        break;
+                }
+
+                // clear error on server
+                Server.ClearError();
+
+                Response.Redirect(String.Format("~/Error/{0}/?message={1}", action, exception.Message));
+            }
         }
     }
 }
